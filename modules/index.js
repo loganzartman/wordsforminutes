f11dcd95acd86fafdc72d44475f9f847828381fe
Logan Zartman <logan.zartman@utexas.edu>
2020-08-21 03:08:11 -0500

    add punctuation + caps options

--- modules/index.js
+++ modules/index.js
@@ -4,11 +4,35 @@ import TextInput from "./TextInput.js";
 
 const App = (props) => {
   const [coherence, setCoherence] = useState(8);
+  const [punctuation, setPunctuation] = useState(true);
+  const [caps, setCaps] = useState(true);
 
   const [words, refreshWords] = useWords({
     coherence,
+    punctuation,
+    caps,
     length: 500
   });
+
+  const controls = html`
+    <button class="reset-button" onClick=${() => refreshWords()}>reset</button>
+    <label>
+      <input
+        type="checkbox"
+        checked=${punctuation}
+        onChange=${(e) => setPunctuation(e.target.checked)}
+      />
+      punctuation
+    </label>
+    <label>
+      <input
+        type="checkbox"
+        checked=${caps}
+        onChange=${(e) => setCaps(e.target.checked)}
+      />
+      caps
+    </label>
+  `;
 
   return html`
     <div class="main-content">
@@ -23,7 +47,7 @@ const App = (props) => {
         />
         <label for="coherence">coherence: ${coherence}</label>
       </div>
-      <button class="reset-button" onClick=${() => refreshWords()}>aaah</button>
+      ${controls}
       <div class="words-area">
         ${words}
       </div>
--- modules/useWords.js
+++ modules/useWords.js
@@ -1,4 +1,4 @@
-import {useState, useMemo, useCallback} from "../node_modules/htm/preact/standalone.module.js";
+import {useState, useMemo, useEffect, useCallback} from "../node_modules/htm/preact/standalone.module.js";
 import {ChainBuilder, TextGenerator} from "./markov.js";
 import {normalizeSentence} from "./text.js";
 import corpus from "./pride_and_prejudice.js";
@@ -9,7 +9,7 @@ const sourceWords = corpus
   .replace(/[\d\[\]()_]/g, "")
   .split("");
 
-export default function useWords({coherence=8, length=100}={}) {
+export default function useWords({coherence=8, length=100, punctuation=true, caps=true}={}) {
   const generator = useMemo(() => {
     // really dumb markov chain building
     const builder = new ChainBuilder(coherence);
@@ -19,11 +19,23 @@ export default function useWords({coherence=8, length=100}={}) {
   }, [coherence]);
 
   const generate = useCallback(
-  	() => Array.from(generator.getSentence(length)).join(""),
-  	[generator]
+  	() => {
+  		let w = Array.from(generator.getSentence(length)).join("");
+  		if (!punctuation)
+  			w = w.replace(/\p{P}/ug, "");
+  		if (!caps)
+  			w = w.toLocaleLowerCase();
+  		return w;
+  	},
+  	[generator, punctuation, caps]
  	);
 
-	const [words, setWords] = useState(generate());
+ 	useEffect(
+ 		() => setWords(generate()),
+ 		[punctuation, caps]
+ 	);
+
+	const [words, setWords] = useState();
 	
 	const refreshWords = () => setWords(generate());
 	return [words, refreshWords];
