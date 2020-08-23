be6e002f78f4b9f53319990cf8f7c005ffc86737
Logan Zartman <logan.zartman@utexas.edu>
2020-08-22 21:31:28 -0500

    smooth scroll :O

--- modules/WordScroller.js
+++ modules/WordScroller.js
@@ -1,13 +1,20 @@
-import {html, useState} from "https://unpkg.com/htm/preact/standalone.mjs?module";
+import {html, useState, useRef, useLayoutEffect} from "https://unpkg.com/htm/preact/standalone.mjs?module";
 
 export default function WordScroller({words=[], prevWord=""}={}) {
+  const [scrollDistance, setScrollDistance] = useState(0);
+  const firstWordRef = useRef();
+  const scrollerRef = useRef();
+
   const wordsElems = words.map((word, i) => {
     const classes = ["scroller-word"];
     if (i === 0)
       classes.push("scroller-word-active");
 
     return html`
-      <div class=${classes.join(" ")}>
+      <div 
+        ref=${i === 0 ? firstWordRef : undefined}
+        class=${classes.join(" ")}
+      >
         ${word}
       </div>
     `
@@ -19,8 +26,19 @@ export default function WordScroller({words=[], prevWord=""}={}) {
     </div>
   `;
 
+  // smooth scroll effect
+  useLayoutEffect(() => {
+    scrollerRef.current.style.transition = "none"; // don't animate
+    scrollerRef.current.style.transform = `translateX(${scrollDistance}px)`;
+    scrollerRef.current.offsetWidth; // force reflow
+    scrollerRef.current.style.transition = null; // do animate
+    scrollerRef.current.style.transform = `translateX(0)`;
+
+    setScrollDistance(firstWordRef.current.offsetWidth);
+  }, [words]);
+
   return html`
-    <div class="scroller-words">
+    <div ref=${scrollerRef} class="scroller-words">
       ${prevWordEl}
       ${wordsElems}
     </div>
