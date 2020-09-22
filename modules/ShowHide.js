a748bbea5a8796b0dafe87134d7d78d61b987872
nondefault <spam@nondefault.net>
2020-09-21 22:15:07 -0500

    improve ShowHide (actually collapses)

--- modules/ShowHide.js
+++ modules/ShowHide.js
@@ -1,8 +1,37 @@
-import {html} from "./preact.js";
+import {html, useEffect, useRef} from "./preact.js";
 
-export default function ShowHide({hidden, children}={}) {
+export default function ShowHide({hidden, children, duration=200}={}) {
+  const containerRef = useRef();
+  const timeoutRef = useRef(null);
+
+  useEffect(() => {
+    const container = containerRef.current;
+    if (!container)
+      return;
+    
+    if (timeoutRef.current !== null) {
+      clearTimeout(timeoutRef.current);
+      timeoutRef.current = null;
+    }
+      
+    if (hidden) {
+      container.className = "show-hide show-hide-hidden";
+      timeoutRef.current = setTimeout(() => {
+        container.style.display = "none";
+      }, duration);
+    } else {
+      container.style.display = "inherit";
+      container.className = "show-hide show-hide-hidden";
+      container.offsetWidth; // force reflow
+      container.className = "show-hide";
+    }
+  }, [hidden]);
+
   return html`
-    <div class=${"show-hide" + (hidden ? " show-hide-hidden" : "")} >
+    <div 
+      ref=${containerRef}
+      style=${{"transition-duration": `${duration}ms`}}
+    >
       ${children} 
     </div>
   `;
